f54220ceb34fc8f13f0e7b1f74dcac30a2cd19fd
Chianson <chanmancu@gmail.com>
2018-03-09 12:23:03 -0800

    Fix chianson's merge

--- homelessResources.js
+++ homelessResources.js
@@ -17,107 +17,106 @@
             center: [-122.30156565, 47.6011886],
             zoom: 16,
             pitch: 120,
-            duration: 5000
+            duration: 4000
         },
         'Multi-Service-Center:Federal-Way': {
 
-            bearing: 90,
+            bearing: 0,
             center: [-122.318042154362, 47.3007686442953],
-            zoom: 13,
-            pitch: 30,
-
-            duration: 5000
+            zoom: 16,
+            pitch: 0,
+            duration: 4000
         },
         'YWCA:Renton': {
             bearing: -90,
             center: [-122.203563458549, 47.4814093],
             zoom: 16,
             pitch: 90,
-            duration: 5000
+            duration: 4000
         },
         'Solid-Ground:North-Seattle': {
             bearing: 235,
             center: [-122.332551801326, 47.69870805],
             zoom: 13,
             pitch: 90,
-            duration: 5000
+            duration: 4000
         },
         'YMCA-Young-Adult-Services-Drop-in-Center': {
-            bearing: 1,
-            center: [-122.3303895, 47.6182332],
-            zoom: 18,
-            pitch: 1,
-            duration: 1
+            bearing: 235,
+            center: [-122.301253, 47.5841884],
+            zoom: 16,
+            pitch: 90,
+            duration: 4000
         },
         'YouthCare’s-James-W.-Ray-Orion-Center': {
-            bearing: 1,
+            bearing: -45,
             center: [-122.3303895, 47.6182332],
-            zoom: 18,
-            pitch: 1,
-            duration: 1
+            zoom: 16,
+            pitch: 90,
+            duration: 4000
         },
         'Peace-for-the-Streets-by-Kids-from-the-Streets': {
-            bearing: 1,
+            bearing: -90,
             center: [-122.3077338, 47.6155844],
-            zoom: 18,
-            pitch: 1,
-            duration: 1
+            zoom: 17,
+            pitch: 90,
+            duration: 4000
         },
         'Nexus-Youth-&-Families': {
-            bearing: 1,
+            bearing: 0,
             center: [-122.218837787879, 47.2995776464646],
             zoom: 18,
-            pitch: 1,
-            duration: 1
+            pitch: 0,
+            duration: 4000
         },
         'Teen-Feed': {
-            bearing: 1,
+            bearing: 155,
             center: [-122.31273440028, 47.66432385],
-            zoom: 18,
-            pitch: 1,
-            duration: 1
+            zoom: 19,
+            pitch: 90,
+            duration: 4000
         },
         'University-District-Youth-Center': {
-            bearing: 1,
+            bearing: -10,
             center: [-122.311670474999, 47.66185935],
             zoom: 18,
-            pitch: 1,
-            duration: 1
+            pitch: 90,
+            duration: 4000
         },
         'Catholic-Community-Services:Bellevue': {
-            bearing: 1,
+            bearing: 0,
             center: [-122.192249916315, 47.6114129],
             zoom: 18,
-            pitch: 1,
-            duration: 1
+            pitch: 0,
+            duration: 4000
         },
         'New-Bethlehem-Day-Center:Kirkland': {
-            bearing: 1,
+            bearing: 0,
             center: [-122.18098627459, 47.6758031],
             zoom: 18,
-            pitch: 1,
-            duration: 1
+            pitch: 0,
+            duration: 4000
         },
         'New-Horizons': {
-            bearing: 1,
+            bearing: 235,
             center: [-122.349536087159, 47.6171479],
             zoom: 18,
-            pitch: 1,
-            duration: 1
+            pitch: 90,
+            duration: 4000
         },
         'Friends-of-Youth-Redmond': {
-            bearing: 1,
+            bearing: 0,
             center: [-122.122910653061, 47.6797058163265],
             zoom: 18,
-            pitch: 1,
-            duration: 1
+            pitch: 0,
+            duration: 4000
         },
         'VA-Puget-Sound’s-Community-Housing-and-Outreach-Services': {
-            bearing: 1,
+            bearing: 90,
             center: [-122.2109979, 47.4810182],
-            zoom: 18,
-            pitch: 1,
-            duration: 1
+            zoom: 16,
+            pitch: 90,
+            duration: 4000
         }
     };
     
@@ -178,6 +177,8 @@
                 }
             }, labelLayerId);
         });
+        loadLegend();
+        loadPopUps();
     };
     
     /**
@@ -276,6 +277,67 @@
         var element = document.getElementById(id); 
         var bounds = element.getBoundingClientRect(); 
         return bounds.top < window.innerHeight && bounds.bottom > 0;
+    }
+    
+    function loadPopUps(){
+        // add pop up for homeless shelters 
+        map.on('click', 'homeless-shelters', function (e) {
+        var coordinates = e.features[0].geometry.coordinates.slice();
+        var address = e.features[0].properties.address;
+        var address_no_space = address.replace(/ /gi, '+');
+        var name = e.features[0].properties.name;
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+        new mapboxgl.Popup()
+            .setLngLat(coordinates)
+            .setHTML('<div align = "center">' + '<h3>' + name + '</h3>' + '<p> Address: '+ 
+            	'<a href = "http://www.google.com/maps/place/' + address_no_space + '/">' + address + '</a>' + '</p>' + '</div>')
+            .addTo(map);
+     });
+
+        // add popup for foodbank
+        map.on('click', 'food-banks', function (e) {
+        var coordinates = e.features[0].geometry.coordinates.slice();
+        var address = e.features[0].properties.Address;
+        var address_no_space = address.replace(/ /gi, '+');
+        var name = e.features[0].properties.Name;
+        var website = e.features[0].properties.Website;
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+        new mapboxgl.Popup()
+            .setLngLat(coordinates)
+            .setHTML('<div align = "center">' + '<h3>' + name + '</h3>' + '<p> Address: '+ '<a href = "http://www.google.com/maps/place/' + address_no_space + '/">' + address + '</a>' + '<br> Website: ' + '<a href = ' + website + '>' + website + '<a/>' + '</p>' + '</div>')
+            .addTo(map);
+        });
+
+        // change cursor when its at a marker 
+        map.on('mouseenter', 'homeless-shelters', function () {map.getCanvas().style.cursor = 'pointer';});
+        map.on('mouseenter', 'food-banks', function () {map.getCanvas().style.cursor = 'pointer';});
+        // change it back to a pointer when it leaves.
+        map.on('mouseleave', 'homeless-shelters', function () {map.getCanvas().style.cursor = '';});
+        map.on('mouseleave', 'food-banks', function () {map.getCanvas().style.cursor = '';});
+        
+        var layers = ['Food Bank', 'Homeless Shelters'];
+        var images = ["url('https://github.com/Chianson/geog458_Final/blob/master/data/icon/food_icon.svg')", 
+                        "url('https://github.com/Chianson/geog458_Final/blob/master/data/icon/shelter_icon.svg')"];
+        for (var i = 0; i < layers.length; i++) {
+            var layer = layers[i];
+            var image = images[i];
+            var item = document.createElement('div');
+            var key = document.createElement('span');
+            key.className = 'legend-key';
+            key.style.backgroundImage = image;
+            var value = document.createElement('span');
+            value.innerHTML = layer;
+            item.appendChild(key);
+            item.appendChild(value);
+            legend.appendChild(item);
+          
+        var item = document.createElement('div'); 
+       	var value = document.createElement('span');
+        }
     }
 })();
 
